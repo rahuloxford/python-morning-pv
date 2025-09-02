e76c0e027c2b6e5d66d5895a42fe0fc31a021031
rahuloxford <iamrahul.oxford@gmail.com>
2025-09-02 18:12:18 +0530

    django started

---
+++ harshita/django-masterclass/myapp/myapp/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("<h1>Welcome to our home page</h1>")
+
+def about(request):
+    return HttpResponse("<h1>Welcome to our about page</h1>")
+
+def contact(request):
+    return HttpResponse("<h1>Welcome to our contact page</h1>")
