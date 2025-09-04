40631d8eb805fe7218759b4b28891a6b6af826ad
rahuloxford <iamrahul.oxford@gmail.com>
2025-09-04 18:08:45 +0530

    django masterclass part 2 - template & static

--- harshita/django-masterclass/myapp/myapp/views.py
+++ harshita/django-masterclass/myapp/myapp/views.py
@@ -1,10 +1,14 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("<h1>Welcome to our home page</h1>")
+    # return HttpResponse("<h1>Welcome to our home page</h1>")
+    return render(request, "website/index.html")
 
 def about(request):
-    return HttpResponse("<h1>Welcome to our about page</h1>")
+    # return HttpResponse("<h1>Welcome to our about page</h1>")
+    return render(request, "website/about.html")
 
 def contact(request):
-    return HttpResponse("<h1>Welcome to our contact page</h1>")
+    # return HttpResponse("<h1>Welcome to our contact page</h1>")
+    return render(request, "website/contact.html")
